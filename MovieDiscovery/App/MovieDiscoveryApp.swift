f0dded872d7b4742593bda3db4e3f252be44027e
Keshav Raj <keshv.raj@yahoo.com>
2026-08-06 12:14:17 +0530

    chore(app): organize application entry point
    -Move MovieDiscoveryApp into App
    -Rename ContentView to RootView
    -Move RootView into App
    -Remove SwiftData template boilerplate

---
+++ MovieDiscovery/App/MovieDiscoveryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieDiscoveryApp.swift
+//  MovieDiscovery
+//
+//  Created by Keshav Raj on 06/08/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieDiscoveryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            RootView()
+        }
+    }
+}
---
+++ MovieDiscovery/App/RootView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  MovieDiscovery
+//
+//  Created by Keshav Raj on 06/08/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct RootView: View {
+    
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    RootView()
+}
