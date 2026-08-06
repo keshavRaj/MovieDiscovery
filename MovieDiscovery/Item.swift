4806cd3b7ad4dbb3cd939f8f082ba8b7c756879d
Keshav Raj <keshv.raj@yahoo.com>
2026-08-06 12:00:43 +0530

    Initial Project commit

---
+++ MovieDiscovery/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  MovieDiscovery
+//
+//  Created by Keshav Raj on 06/08/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
